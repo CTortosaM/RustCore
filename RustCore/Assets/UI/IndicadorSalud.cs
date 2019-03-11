e29203f260ad2aacf5f43857784e33e8cb2916a2
AdrHer13 <36443291+AdrHer13@users.noreply.github.com>
2019-03-11 19:36:02 +0100

    AI and Damage

---
+++ RustCore/Assets/UI/IndicadorSalud.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class IndicadorSalud : MonoBehaviour
+{
+    private Text Vida;
+    private int vidaRestante;
+
+    public void Actualizar(int vidaPerdida)
+    {
+        VidaRestante -= vidaPerdida;
+        if (VidaRestante < 0) VidaRestante = 0;
+        Vida.text = VidaRestante.ToString() + " PV";
+    }
+
+    public int VidaRestante
+    {
+        get => vidaRestante;
+        set => vidaRestante = value;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vida = GetComponent<Text>();
+        VidaRestante = 500;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
