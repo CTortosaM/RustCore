2ab08faf2cbe61ae689f501367e2dc1c42766670
CTortosaM <36443287+CTortosaM@users.noreply.github.com>
2019-03-20 17:34:44 +0100

    Import de pistola a Unity

---
+++ RustCore/Assets/Armas/Initial pistol/InitialP.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InitialP : MonoBehaviour
+{
+
+    public int maxAmmoPerMagazine = 12;
+    public int bulletsLeft;
+    private Text text;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        bulletsLeft = maxAmmoPerMagazine;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
