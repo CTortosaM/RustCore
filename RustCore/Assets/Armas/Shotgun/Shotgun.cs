c0181616d50a1ddcf61429d7ba93aebd04dbd871
Víctor White <36444816+vicblab1@users.noreply.github.com>
2019-04-01 23:50:47 +0200

    Escopeta colocada en el pickup

---
+++ RustCore/Assets/Armas/Shotgun/Shotgun.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Animations;
+
+[RequireComponent(typeof(AmmoCount))]
+public class Shotgun : MonoBehaviour
+{
+
+    private AmmoCount ammo;
+    [SerializeField] private float shootInterval = 1f;
+    private float nextPossibleShootTime;
+
+    [SerializeField] private int damage = 50;
+
+    public Text text;
+    private bool canShoot;
+   // private Animator animator;
+
+    [SerializeField] private Camera camera;
+
+    [SerializeField] private float range = 100f;
+    [SerializeField] private ParticleSystem muzzleFlash;
+
+    //private bool isReloading = false;
+
+
+    public int Damage { get => damage; set => damage = value; }
+    public float ShootInterval { get => shootInterval; set => shootInterval = value; }
+
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        ammo = GetComponent<AmmoCount>();
+        text.text = ammo.AmmoLeftInMagazine.ToString();
+      //  animator = GetComponent<Animator>();
+        canShoot = true;
+        nextPossibleShootTime = Time.time;
+        ammo.updateAmmoText();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)  && canShoot && Time.time >= nextPossibleShootTime)
+        {
+            if (ammo.AmmoLeftInMagazine > 0)
+            {
+                nextPossibleShootTime = Time.time + ShootInterval;
+                ammo.AmmoLeftInMagazine--;
+                ammo.updateAmmoText();
+                Shoot();
+            }
+
+            text.text = ammo.AmmoLeftInMagazine.ToString();
+        }
+
+
+        if (Input.GetKeyDown(KeyCode.R) && ammo.AmmoLeftInMagazine < ammo.MaxAmmoPerMagazine && ammo.TotalAmmo != 0)
+        {
+           // StartCoroutine(reload());
+
+        }
+    }
+
+
+   /* private IEnumerator reload()
+    {
+        isReloading = true;
+        animator.SetBool("isReloading", true);
+
+        yield return new WaitForSeconds(1);
+
+        ammo.reload();
+
+        isReloading = false;
+        //ammo.AmmoLeftInMagazine = 12;
+        text.text = ammo.AmmoLeftInMagazine.ToString();
+        animator.SetBool("isReloading", false);
+    }*/
+
+
+    private void Shoot()
+    {
+
+        RaycastHit hit;
+
+        muzzleFlash.Play();
+
+        if (Physics.Raycast(camera.transform.position, camera.transform.forward, out hit, range))
+        {
+            if (hit.collider.gameObject.tag == "Enemy")
+            {
+                hit.collider.gameObject.GetComponent<AIEnemigo>().Actualizar(Damage);
+            }
+        }
+
+    }
+
+    void OnEnable()
+    {
+        ammo.updateAmmoText();
+    }
+}
+
