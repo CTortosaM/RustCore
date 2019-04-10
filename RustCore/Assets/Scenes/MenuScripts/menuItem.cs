a84b33390e0967162fa5e787f935baf10b0ba0c3
CTortosaM <CTortosaM@users.noreply.github.com>
2019-04-10 23:05:22 +0200

    Highlight en botones

---
+++ RustCore/Assets/Scenes/MenuScripts/menuItem.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class menuItem : MonoBehaviour
+{
+    private Text text;
+    [SerializeField] private Color originalColor;
+    [SerializeField] private Color highlightColor;
+    // Start is called before the first frame update
+    void Start()
+    {
+       text = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public void highlight(bool highlight)
+    {
+        
+        if (highlight)
+        {
+            text.color = highlightColor;
+            return;
+        }
+
+        text.color = originalColor;
+    }
+
+    
+}
