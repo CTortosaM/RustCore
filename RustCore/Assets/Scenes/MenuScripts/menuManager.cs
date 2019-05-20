8dcbd1eb9f57f87e14caa173f56dd71d6fc06067
CTortosaM <36443287+CTortosaM@users.noreply.github.com>
2019-05-20 17:22:48 +0200

    Fixed pause

--- RustCore/Assets/Scenes/MenuScripts/menuManager.cs
+++ RustCore/Assets/Scenes/MenuScripts/menuManager.cs
@@ -35,6 +35,10 @@ public class menuManager: MonoBehaviour
     public void LoadLevel()
     {
         SceneManager.LoadSceneAsync("SalaHub");
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        Time.timeScale = 1f;
+        PauseManager.isPaused = false;
     }
 
 
