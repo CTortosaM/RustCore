c1880a79b6e320b1b97f16be4bec28a14a04bdd8
CTortosaM <CTortosaM@users.noreply.github.com>
2019-05-29 20:38:56 +0200

    Dash Icon animation

---
+++ RustCore/Assets/UI/DashCooldownIcon/DashIcon.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DashIcon : MonoBehaviour
+{
+    private Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        PlayerController.onPlayerDash += startAnimation;
+    }
+
+    private void startAnimation()
+    {
+        if (animator.Equals(null)) animator = FindObjectOfType<DashIcon>().gameObject.GetComponent<Animator>();
+        animator.Play("DashExecuted");
+    }
+    
+
+    
+}
